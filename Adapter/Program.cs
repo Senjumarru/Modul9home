870e6d7032ef54a6e37eff1819c3c79969fafb22
Senjumarru <alinursarsekov315@gmail.com>
2024-11-07 00:22:19 +0500

    Добавьте файлы проекта.

---
+++ Adapter/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+internal class PaymentIntegrationSystem
+{
+    public interface IPaymentProcessor
+    {
+        void ProcessPayment(double amount);
+    }
+
+    public class PayPalPaymentProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(double amount)
+        {
+            Console.WriteLine($"Обработка платежа через PayPal: {amount} рублей");
+        }
+    }
+
+    public class StripePaymentService
+    {
+        public void MakeTransaction(double totalAmount)
+        {
+            Console.WriteLine($"Обработка платежа через Stripe: {totalAmount} рублей");
+        }
+    }
+
+    public class StripePaymentAdapter : IPaymentProcessor
+    {
+        private readonly StripePaymentService stripeService;
+
+        public StripePaymentAdapter(StripePaymentService stripeService)
+        {
+            this.stripeService = stripeService;
+        }
+
+        public void ProcessPayment(double amount)
+        {
+            stripeService.MakeTransaction(amount);
+        }
+    }
+
+    public class SquarePaymentService
+    {
+        public void ExecutePayment(double payment)
+        {
+            Console.WriteLine($"Обработка платежа через Square: {payment} рублей");
+        }
+    }
+
+    public class SquarePaymentAdapter : IPaymentProcessor
+    {
+        private readonly SquarePaymentService squareService;
+
+        public SquarePaymentAdapter(SquarePaymentService squareService)
+        {
+            this.squareService = squareService;
+        }
+
+        public void ProcessPayment(double amount)
+        {
+            squareService.ExecutePayment(amount);
+        }
+    }
+
+    public class YandexPaymentService
+    {
+        public void CompletePayment(double amount)
+        {
+            Console.WriteLine($"Обработка платежа через Yandex: {amount} рублей");
+        }
+    }
+
+    public class YandexPaymentAdapter : IPaymentProcessor
+    {
+        private readonly YandexPaymentService yandexService;
+
+        public YandexPaymentAdapter(YandexPaymentService yandexService)
+        {
+            this.yandexService = yandexService;
+        }
+
+        public void ProcessPayment(double amount)
+        {
+            yandexService.CompletePayment(amount);
+        }
+    }
+
+    public static void Main()
+    {
+        IPaymentProcessor paypalProcessor = new PayPalPaymentProcessor();
+        paypalProcessor.ProcessPayment(150.0);
+
+        IPaymentProcessor stripeProcessor = new StripePaymentAdapter(new StripePaymentService());
+        stripeProcessor.ProcessPayment(200.0);
+
+        IPaymentProcessor squareProcessor = new SquarePaymentAdapter(new SquarePaymentService());
+        squareProcessor.ProcessPayment(300.0);
+
+        IPaymentProcessor yandexProcessor = new YandexPaymentAdapter(new YandexPaymentService());
+        yandexProcessor.ProcessPayment(350.0);
+    }
+}
---
+++ Modul9home/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+internal class CafeOrderSystem
+{
+    public interface IBeverage
+    {
+        string GetDescription();
+        double Cost();
+    }
+
+    public class Espresso : IBeverage
+    {
+        public string GetDescription() => "Эспрессо";
+        public double Cost() => 2.0;
+    }
+
+    public class Tea : IBeverage
+    {
+        public string GetDescription() => "Чай";
+        public double Cost() => 1.5;
+    }
+
+    public class Latte : IBeverage
+    {
+        public string GetDescription() => "Латте";
+        public double Cost() => 2.5;
+    }
+
+    public class Mocha : IBeverage
+    {
+        public string GetDescription() => "Мокка";
+        public double Cost() => 3.0;
+    }
+
+    public abstract class BeverageDecorator : IBeverage
+    {
+        protected IBeverage beverage;
+
+        protected BeverageDecorator(IBeverage beverage)
+        {
+            this.beverage = beverage;
+        }
+
+        public virtual string GetDescription() => beverage.GetDescription();
+        public virtual double Cost() => beverage.Cost();
+    }
+
+    public class Milk : BeverageDecorator
+    {
+        public Milk(IBeverage beverage) : base(beverage) { }
+
+        public override string GetDescription() => $"{beverage.GetDescription()} с молоком";
+        public override double Cost() => beverage.Cost() + 0.5;
+    }
+
+    public class Sugar : BeverageDecorator
+    {
+        public Sugar(IBeverage beverage) : base(beverage) { }
+
+        public override string GetDescription() => $"{beverage.GetDescription()} с сахаром";
+        public override double Cost() => beverage.Cost() + 0.2;
+    }
+
+    public class WhippedCream : BeverageDecorator
+    {
+        public WhippedCream(IBeverage beverage) : base(beverage) { }
+
+        public override string GetDescription() => $"{beverage.GetDescription()} с взбитыми сливками";
+        public override double Cost() => beverage.Cost() + 0.7;
+    }
+
+    public class Syrup : BeverageDecorator
+    {
+        public Syrup(IBeverage beverage) : base(beverage) { }
+
+        public override string GetDescription() => $"{beverage.GetDescription()} с сиропом";
+        public override double Cost() => beverage.Cost() + 0.6;
+    }
+
+    public static void Main()
+    {
+        IBeverage beverage = new Espresso();
+        beverage = new Milk(beverage);
+        beverage = new Sugar(beverage);
+        beverage = new WhippedCream(beverage);
+        Console.WriteLine($"{beverage.GetDescription()}: {beverage.Cost()} рублей");
+
+        IBeverage beverage2 = new Mocha();
+        beverage2 = new Syrup(beverage2);
+        beverage2 = new WhippedCream(beverage2);
+        Console.WriteLine($"{beverage2.GetDescription()}: {beverage2.Cost()} рублей");
+    }
+}
